b756fdb7f1cacf9d7aeae9043b8c6f6014dfe570
Tjeerd Hes <tjeerd.hes@amsteldiep.nl>
2022-07-24 10:56:18 +0200

    [r2r][p 22-23] forHtml (for is reserved), use message object

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 
-const r = "react";
+const m = { // message
+    h: "Hello",
+    r: "react",
+    w: "world"
+};
 
 function App() {
   return (
     <main>
       <header>
-        <h1>Hello {r} world.</h1>
+        <h1>{m.h} {m.r} {m.w}.</h1>
       </header>
+      <section>
+            <label>
+                search
+                <input type="text" />
+                (label around)
+            </label>
+      </section>
+      <section>
+          <label htmlFor="search2">search again (label above) </label>
+          <input id="search2" type="text" />
+      </section>
     </main>
   );
 }

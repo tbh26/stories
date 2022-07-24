de87048d35c33cd451b5b3146560b1e9b962f210
Tjeerd Hes <tjeerd.hes@amsteldiep.nl>
2022-07-24 22:00:32 +0200

    [r2r][p 40] import createRoot

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const appContainer = document.getElementById('root');
+const app = createRoot(appContainer);
+app.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>

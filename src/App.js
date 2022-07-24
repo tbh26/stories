d4fc0aef07ab69cccf54f0d45cedb84fb53f0496
Tjeerd Hes <tjeerd.hes@amsteldiep.nl>
2022-07-24 14:14:08 +0200

    [r2r][p 24] use function call

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
-
-const m = { // message
-    h: "Hello",
-    r: "react",
-    w: "world"
-};
-
 function App() {
-  return (
-    <main>
-      <header>
-        <h1>{m.h} {m.r} {m.w}.</h1>
-      </header>
-      <section>
-            <label>
-                search
-                <input type="text" />
-                (label around)
-            </label>
-      </section>
-      <section>
-          <label htmlFor="search2">search again (label above) </label>
-          <input id="search2" type="text" />
-      </section>
-    </main>
-  );
+    function echoFun(mesg) {
+        return mesg;
+    }
+
+    return (
+        <main>
+            <header>
+                <h1>Hello {echoFun('react')} world.</h1>
+            </header>
+            <section>
+                <label>
+                    search
+                    <input type="text"/>
+                </label>
+            </section>
+        </main>
+    );
 }
 
 export default App;

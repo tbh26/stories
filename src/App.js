2f1f4cd20b964057283d37d9bca0ec46942c95a1
Tjeerd Hes <tjeerd.hes@amsteldiep.nl>
2022-07-24 17:04:43 +0200

    [r2r][p 26-28] use list in jsx, part I

--- src/App.js
+++ src/App.js
@@ -1,3 +1,22 @@
+const list = [
+    {
+        title: 'React',
+        url: 'https://reactjs.org/',
+        author: 'Jordan Walke',
+        num_comments: 3,
+        points: 4,
+        objectID: 0,
+    },
+    {
+        title: 'Redux',
+        url: 'https://redux.js.org/',
+        author: 'Dan Abramov, Andrew Clark',
+        num_comments: 2,
+        points: 5,
+        objectID: 1,
+    },
+];
+
 function App() {
     function echoFun(mesg) {
         return mesg;
@@ -13,6 +32,13 @@ function App() {
                     search
                     <input type="text"/>
                 </label>
+            </section>
+            <section>
+                <ul>
+                    {list.map(function (item) {
+                        return <li key={item.objectID}>{item.title}</li>;
+                    })}
+                </ul>
             </section>
         </main>
     );

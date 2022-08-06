eeceb7b85839394f24854c4919576a8ed5a0c43e
Tjeerd Hes <tjeerd.hes@amsteldiep.nl>
2022-08-06 15:25:00 +0200

    [r2r][p 45-47] introduce props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-const list = [
-    {
-        title: 'React',
-        url: 'https://reactjs.org/',
-        author: 'Jordan Walke',
-        num_comments: 3,
-        points: 4,
-        objectID: 0,
-    },
-    {
-        title: 'Redux',
-        url: 'https://redux.js.org/',
-        author: 'Dan Abramov, Andrew Clark',
-        num_comments: 2,
-        points: 5,
-        objectID: 1,
-    },
-];
 
 const App = () => {
+
+    const techStuff = [
+        {
+            title: 'React',
+            url: 'https://reactjs.org/',
+            author: 'Jordan Walke II',
+            num_comments: 3,
+            points: 4,
+            objectID: 0,
+        },
+        {
+            title: 'Redux',
+            url: 'https://redux.js.org/',
+            author: 'Dan Abramov, Andrew Clark',
+            num_comments: 2,
+            points: 5,
+            objectID: 1,
+        },
+    ];
+
     function echoFun(mesg) {
         return mesg;
     }
@@ -32,11 +34,11 @@ const App = () => {
             </section>
             <hr/>
             <section>
-                <List/>
+                <List list={techStuff} />
             </section>
             <hr/>
             <section>
-                <List/>
+                <List list={techStuff} />
             </section>
             <hr/>
             <section>
@@ -60,9 +62,9 @@ const Search = () => {
     );
 };
 
-const List = () => (
+const List = (props) => (
     <ul>
-        {list.map(item => (
+        {props.list.map(item => (
                 <li key={item.objectID}>
                                 <span>
                                     <a href={item.url}>{item.title}</a>

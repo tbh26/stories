b8a347cb5428923fa12d762aea82e6dcd6b19eca
Tjeerd Hes <tjeerd.hes@amsteldiep.nl>
2022-08-20 15:15:24 +0200

    [r2r][-] remove reportWebVitals

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const appContainer = document.getElementById('root');
 const app = createRoot(appContainer);
@@ -10,8 +9,3 @@ app.render(
     // <React.StrictMode><App /></React.StrictMode>
     <App/>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
